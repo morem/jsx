cff962ba9f359ce605fe19ce150d444c603e2b69
morem <morem@126.com>
2015-03-23 08:52:55 +0800

    1. 添加不同PC与机子的生产配置文件

---
+++ config_file/HT2512_1/生产环境.bat
@@ -0,0 +1,5 @@
+echo WORK_DIRECTORY=%cd%> C:\photoshop.cfg
+echo CONFIG_DIRECTORY=E:/360ÔÆÅÌ/Éú²ú/ÅäÖÆ/>> C:\photoshop.cfg
+echo PIC_LIB=E:/360ÔÆÅÌ/003 Í¼¿â±àºÅ/morem/>> C:\photoshop.cfg
+echo MACHINE_NO=HT2512_1>> C:\photoshop.cfg
+
