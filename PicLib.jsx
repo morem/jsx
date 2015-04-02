72f2ba9e3eaf1b8137ba26fdf092eb5288eccd4d
morem <morem@126.com>
2015-04-02 10:19:33 +0800

    1. 修改图库文件生成函数 2. 添加专门用于生成查找图的函数 3. 把功能执行运行文件单独放入work_picLib.jsx 4. 调整PicLib.jsx中的函数为基础函数，不再调用更高层中的函数

--- PicLib.jsx
+++ PicLib.jsx
@@ -9,6 +9,8 @@
 #include "csv.jsx"
 #include "jigsaw.jsx"
 #include "pinyin.jsx"
+#include "log.jsx"
+#include "caseInfo.jsx"
 
 var imageArray = new Array ();
 
@@ -83,16 +85,20 @@ function PicLib_GetFileModifiedTimeGMTMs (path)
 	if (!file.exists)return 0;
 	return Date.parse (file.modified);
 }
+function PicLib_GetModulFromCaseID(caseID)
+{
+    return caseID.slice(0, caseID.indexOf("_") )
+}
 
 function PicLib_GetMostFitPicPath (caseID, dirName)
 {
 	var ret = null;
 	var path = null;
 	var p = 48;
-	path = dirName + "/" + PM_GetModulFromCaseID(caseID) + ".tif";
+	path = dirName + "/" + PicLib_GetModulFromCaseID(caseID) + ".tif";
 	if (File_CheckFileExist(path))return path;
 	
-	var caseInfo = PM_GetCaseInfo (caseID);
+	var caseInfo = CaseInfo_GetCaseInfo (caseID);
 	var t = caseInfo["主体宽"]/caseInfo["主体长"]*100;
 	if (Utils_ABS (t-48) > Utils_ABS (t-52))
 	{
@@ -106,7 +112,8 @@ function PicLib_GetMostFitPicPath (caseID, dirName)
 
 	if (p == 52) path = dirName + "/" +  "i4s.tif";
 	if (p == 48) path = dirName + "/" +  "i5s.tif";
-	if (File_CheckFileExist(path))return path;	
+	if (File_CheckFileExist(path))return path;
+    LOG_Add_Error("Can't Find the Most Fit Pic In PicLib of Pricture whit caseID:" + caseID + " And Dir is " + dirName)
 	return null;
 
 }
@@ -165,163 +172,4 @@ function PicLib_NumOrNameToPath (caseID, picID)
 	return path;
 }
 
-
-
-function PicLib_BuildElement (path, id , name)
-{
-    var dir = Utils_GetDirectoryPathFromPath (path);
-    var srcPath = PicLib_GetMostFitPicPath("i5s_gt", dir);
-
-    var tTime = PicLib_GetFileModifiedTimeGMTMs (path) ;
-    var srcTime = PicLib_GetFileModifiedTimeGMTMs (srcPath);
-    var templateTime = PicLib_GetFileModifiedTimeGMTMs (PicLib_GetElementTemplate());
-	if (tTime > srcTime && tTime > templateTime)
-	{
-		imageArray.push (path);
-		return ;
-	}
-
-
-    srcPath = PM_ProAdaptCase (srcPath, "i5s_gt");
-
-
-	var file = new File (PicLib_GetElementTemplate());
-    var doc = app.open (file);
-
-    duplicateFrom ( doc, 
-                    srcPath, 
-                    OpenDocumentType.JPEG, 
-                    "e");
-    doc.artLayers["e"].translate(  new UnitValue(doc.artLayers["pos"].bounds[0].as("px"),"px"), 
-                                     new UnitValue(doc.artLayers["pos"].bounds[1].as("px"),'px'));    
-
-    var targetHeight = GetLayerHeight (doc.artLayers["pos"]);
-    var targetWidth = GetLayerWidth (doc.artLayers["pos"]);
-    var orgHeight = GetLayerHeight (doc.artLayers["e"]);
-    var orgWidth = GetLayerWidth (doc.artLayers["e"]);
-    doc.activeLayer.resize(targetWidth/orgWidth*100, targetWidth/ orgWidth*100, AnchorPosition.TOPLEFT);
-    SetTextLayerContexts (doc, "id", id);
-    SetTextLayerContexts (doc, "name", name);
-
-	var id_b = doc.artLayers["id"].visible;
-    
-    if (CheckLayerExist (doc, "id"))
-    {
-		var b = doc.artLayers["id"].visible;
-        HorzMiddleLayerByLayer (doc, doc.layers["area"], doc.layers["id"]); 
-		doc.artLayers["id"].visible = b;
-	}
-
-    if (CheckLayerExist (doc, "name"))
-	{
-		var b = doc.artLayers["name"].visible;
-        HorzMiddleLayerByLayer (doc, doc.layers["area"], doc.layers["name"]); 
-		 doc.artLayers["name"].visible = b;
-	}
-    var element = new File (path);
-    doc.saveAs (element, GetJPGParam(), true);
-    CloseDoc (doc);
-    imageArray.push (path);
-    return 0;
-}
-
-
-function PicLib_PrepareAllElement (path, id , name)
-{
-    //if (false == File_CheckFileExist (path))
-    {
-        PicLib_BuildElement (path, id , name);
-    }
-    /*
-    else{
-        imageArray.push (path);
-    }*/
-}
-
-
-
-function PicLib_Work (sortType)
-{
-	imageArray = new Array ();
-    var arrayFloder = PicLib_GetAllPic ();
-    var index = 0;
-	var nameArray = new Array;
-    var objectArray = new Object;
-    
-    var t = Pinyin.get("Open");
-
-    for (var f in arrayFloder)
-    {
-        var path = arrayFloder [f].fullName;
-        var id =  PicLib_GetID (path);
-        var name =  PicLib_GetName (path);
-        var py = Pinyin.get (name);
-        index ++;
-        nameArray.push (py);
-        objectArray [py] = new Object();
-        objectArray [py].id = id;
-        objectArray [py].name = name;
-        objectArray [py].path = path + "/s.jpg";
-        objectArray [py].path2 = "C:\\tmp\\" + name + " " + py + ".jpg";        
-
-		//if (index == 10)break;
-    }
-
-	if (CompareString(sortType,"pingyin"))
-		nameArray.sort (function(a, b) {return a.localeCompare(b) });
-
-    
-    for (var f in nameArray)
-    {
-        var k = nameArray[f];
-		try 
-	   	{
-		    var file = new File (objectArray[k].path);
-			file.copy (objectArray[k].path2)
-
-	   	}
-    	catch (err)
-	   	{
-//	   		alert (err);
-    	}
-	}
-	
-
-    //var file = new File (src);
-	//file.copy (targetPath)
-
-    for (var f in nameArray)
-    {
-       var k = nameArray[f];
-		try 
-	   	{
-        	PicLib_PrepareAllElement (objectArray[k].path, objectArray[k].id, objectArray[k].name);
-	   	}
-    	catch (err)
-	   	{
-//	   		alert (err);
-    	}
-	}
-	
-    var a = GetImageWidthAddHeightAsPx (PicLib_GetElementTemplate ());
-    
-    Jigsaw_Init (PicLib_GetPicLabPath() + "./../summary/" + sortType + "/",12,4, a[0], a[1]);
-    Jigsaw_Build (imageArray);
-    Jigsaw_End ();
-    
-    
-}
-
-
-//PicLib_Work ("pingyin");
-//PicLib_Work ("number");
-/*
-function work()
-{
-var t = PicLib_GetName ("./M099 kiss me"); 
-var x = t;
-}
-
-work();
-*/
 
---
+++ work_picLib.jsx
@@ -0,0 +1,183 @@
+﻿#include "picLib.jsx"
+#include "PicManager.jsx"
+
+
+
+function WorkPicLib_BuildElement (path, id , name)
+{
+    var dir = Utils_GetDirectoryPathFromPath (path);
+    var srcPath = PicLib_GetMostFitPicPath("i5s_gt", dir);
+    if (null == srcPath)
+    {
+		LOG_Add_Error("Cant find the fit picture in directory with id is " + id);
+        return false;
+    }
+
+    var tTime = PicLib_GetFileModifiedTimeGMTMs (path) ;
+    var srcTime = PicLib_GetFileModifiedTimeGMTMs (srcPath);
+    var templateTime = PicLib_GetFileModifiedTimeGMTMs (PicLib_GetElementTemplate());
+	if (tTime > srcTime && tTime > templateTime)
+	{
+		imageArray.push (path);
+		return true;
+	}
+
+
+    srcPath = PM_GetCasePic (srcPath,"PicLib", "i5s_gt","all");
+
+
+	var file = new File (PicLib_GetElementTemplate());
+    var doc = app.open (file);
+
+    duplicateFrom ( doc, 
+                    srcPath, 
+                    OpenDocumentType.JPEG, 
+                    "e");
+    doc.artLayers["e"].translate(  new UnitValue(doc.artLayers["pos"].bounds[0].as("px"),"px"), 
+                                     new UnitValue(doc.artLayers["pos"].bounds[1].as("px"),'px'));    
+
+    var targetHeight = GetLayerHeight (doc.artLayers["pos"]);
+    var targetWidth = GetLayerWidth (doc.artLayers["pos"]);
+    var orgHeight = GetLayerHeight (doc.artLayers["e"]);
+    var orgWidth = GetLayerWidth (doc.artLayers["e"]);
+    doc.activeLayer.resize(targetWidth/orgWidth*100, targetWidth/ orgWidth*100, AnchorPosition.TOPLEFT);
+    SetTextLayerContexts (doc, "id", id);
+    SetTextLayerContexts (doc, "name", name);
+
+	var id_b = doc.artLayers["id"].visible;
+    
+    if (CheckLayerExist (doc, "id"))
+    {
+		var b = doc.artLayers["id"].visible;
+        HorzMiddleLayerByLayer (doc, doc.layers["area"], doc.layers["id"]); 
+		doc.artLayers["id"].visible = b;
+	}
+
+    if (CheckLayerExist (doc, "name"))
+	{
+		var b = doc.artLayers["name"].visible;
+        HorzMiddleLayerByLayer (doc, doc.layers["area"], doc.layers["name"]); 
+		 doc.artLayers["name"].visible = b;
+	}
+    var element = new File (path);
+    doc.saveAs (element, GetJPGParam(), true);
+    CloseDoc (doc);
+    imageArray.push (path);
+    return true;
+}
+
+
+function WorkPicLib_PrepareAllElement (path, id , name)
+{
+    {
+        WorkPicLib_BuildElement (path, id , name);
+    }
+}
+
+
+
+function WorkPicLib_Work (sortType)
+{
+	imageArray = new Array ();
+    var arrayFloder = PicLib_GetAllPic ();
+    var index = 0;
+	var nameArray = new Array;
+    var objectArray = new Object;
+    
+    var t = Pinyin.get("Open");
+
+    for (var f in arrayFloder)
+    {
+        var path = arrayFloder [f].fullName;
+        var id =  PicLib_GetID (path);
+        var name =  PicLib_GetName (path);
+        var py = Pinyin.get (name);
+        index ++;
+        nameArray.push (py);
+        objectArray [py] = new Object();
+        objectArray [py].id = id;
+        objectArray [py].name = name;
+        objectArray [py].path = path + "/s.jpg";
+        objectArray [py].path2 = PATH_GetPicLibIndexPath () + name + " " + py + ".jpg";        
+
+    }
+
+	if (CompareString(sortType,"pingyin"))
+		nameArray.sort (function(a, b) {return a.localeCompare(b) });
+
+    for (var f in nameArray)
+    {
+	   var k = nameArray[f];
+		WorkPicLib_PrepareAllElement (objectArray[k].path, objectArray[k].id, objectArray[k].name);
+
+	}
+	
+    var a = GetImageWidthAddHeightAsPx (PicLib_GetElementTemplate ());
+    
+    Jigsaw_Init (PicLib_GetPicLabPath() + "./../summary/" + sortType + "/",12,4, a[0], a[1]);
+    Jigsaw_Build (imageArray);
+    Jigsaw_End ();
+    
+    
+}
+
+function WorkPicLib_BuildSearchIndex ()
+{
+	imageArray = new Array ();
+    var arrayFloder = PicLib_GetAllPic ();
+    var index = 0;
+	var nameArray = new Array;
+    var objectArray = new Object;
+    
+    var t = Pinyin.get("Open");
+
+	var picLibIndexPath = PATH_GetPicLibIndexPath ();
+    if (picLibIndexPath == null){
+		var err = "Get Path for Store Picture Index Error";
+        LOG_Add_Error(err);
+        alert (err);
+        return;
+    }
+    
+    for (var f in arrayFloder)
+    {
+        var path = arrayFloder [f].fullName;
+        var id =  PicLib_GetID (path);
+        var name =  PicLib_GetName (path);
+        var py = Pinyin.get (name);
+        index ++;
+        nameArray.push (py);
+        objectArray [py] = new Object();
+        objectArray [py].id = id;
+        objectArray [py].name = name;
+        objectArray [py].path = path + "/s.jpg";
+        objectArray [py].path2 =  picLibIndexPath + name + " " + py + ".jpg";        
+
+    }
+
+
+    for (var f in nameArray)
+    {
+        var k = nameArray[f];
+		var i = 2;
+        while (i --){
+		    var file = new File (objectArray[k].path);
+			if (false == file.exists)
+		    {
+		        LOG_Add_Error(file.error + "   fileName:" + objectArray[k].path);
+				continue;
+		    }
+		    if (false == file.copy (objectArray[k].path2))
+		    {
+		        LOG_Add_Error(file.error + "   fileName:" + objectArray[k].path);
+		        continue;
+		    }
+            break;
+        }
+	}
+}
+
+
+machine_number = GetMachineNumber ();
+//WorkPicLib_Work ("number");
+WorkPicLib_BuildSearchIndex ();
