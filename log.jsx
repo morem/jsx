96fdc5f5157342512e42218d0462f8c8a445d94a
morem <morem@126.com>
2015-04-26 09:55:52 +0800

    1. 优惠LOG功能

--- log.jsx
+++ log.jsx
@@ -6,44 +6,44 @@
 var logFile = null;
 function LOG_Init ()
 {
-	var logPath = PATH_GetLogPath ();
-	var file = new File (logPath);
-	logFile = file;
+    var logPath = PATH_GetLogPath ();
+    var file = new File (logPath);
+    logFile = file;
 }
 
 function LOG_Add (level, log)
 {
-	if (null == logFile)LOG_Init ();
-   	logFile.open ('a');
-	logFile.writeln (log);
+    if (null == logFile)LOG_Init ();
+    logFile.open ('a');
+    logFile.writeln (log);
     logFile.close ();
 }
 function LOG_Add_Error (log)
 {
-	LOG_Add ("Error", log);
+    LOG_Add ("Error", log);
 }
 
 function LOG_Error (log)
 {
-	LOG_Add ("Error", log);
-	//alert (log, "error", true);
+    LOG_Add ("Error", log);
+    //alert (log, "error", true);
 }
 
 
 function LOG_ErrMsgOut (log)
 {
-	LOG_Add ("Error", log);
-	alert (log, "error", true);
+    LOG_Add ("Error", log);
+    alert (log, "error", true);
 }
 
 function LOG_ALERT (stringID, log)
 {
-	//LOG_Add ("Error", log);
-	//alert (log, "error", true);
+    //LOG_Add ("Error", log);
+    //alert (log, "error", true);
 }
 
 function LOG_Add_Info (log)
 {
-	LOG_Add ("Info", log);
+    LOG_Add ("Info", log);
 }
 
