46aa50a376bb1559b068c0a8f81616efe5154760
morem <morem@126.com>
2015-10-05 14:42:22 +0800

    add the method of 1 picture header

--- mHeader.jsx
+++ mHeader.jsx
@@ -22,8 +22,13 @@ function MH_GetCaseInfo ()
     s_init.path = configPath;
     s_init.data_header_index = 0;
     s_init.data_start = 1;
-    s_init.key = "nameOrID";
+    s_init.key = "index";
     s_init.data_header = new Array ();
+
+    var e  = new Object();
+    e.text = "index";
+    e.format = 's';
+    s_init.data_header.push (e);
 
     var e  = new Object();
     e.text = "nameOrID";
@@ -103,19 +108,90 @@ function MH_OpenTemplate(picPath)
     return doc;
 }
 
+function MH_OpenTemplate2(picPath0, picPath1)
+{
+    var templatePath = GetParam ("NEW_TEMPLATE_DIR") + "head_modul_b.tif";
+    var templateFile = new File (templatePath);
+    var doc = app.open(templateFile);
+    var picDoc0 = MH_GetCaseSharp (picPath0); 
+    layer_main_0 = duplicateLayerFrom(doc,"main0",picDoc0,"main");
+
+    orgWidth = GetLayerWidth (layer_main_0);
+    orgHeight = GetLayerHeight(layer_main_0);
+
+    layer_org = doc.artLayers["Pos_left"];
+    targetWidth = GetLayerWidth (layer_org);
+    targetHeight = GetLayerHeight (layer_org);
+    
+    doc.activeLayer.translate ( new UnitValue(layer_org.bounds[0].as("px"),"px"), 
+                                          new UnitValue(layer_org.bounds[1].as("px"),'px'));
+
+    doc.activeLayer.resize(targetWidth/orgWidth*100, targetHeight/ orgHeight*100, AnchorPosition.TOPLEFT);
+    app.doAction ("case", "sys2");
+    CloseDoc(picDoc0);
+
+    var picDoc1 = MH_GetCaseSharp (picPath1);
+    layer_main_1 = duplicateLayerFrom(doc,"main1",picDoc1,"main");
+    orgWidth = GetLayerWidth (layer_main_1);
+    orgHeight = GetLayerHeight(layer_main_1);
+
+    layer_org = doc.artLayers["Pos_right"];
+    targetWidth = GetLayerWidth (layer_org);
+    targetHeight = GetLayerHeight (layer_org);
+    
+    doc.activeLayer.translate ( new UnitValue(layer_org.bounds[0].as("px"),"px"), 
+                                          new UnitValue(layer_org.bounds[1].as("px"),'px'));
+
+    doc.activeLayer.resize(targetWidth/orgWidth*100, targetHeight/ orgHeight*100, AnchorPosition.TOPLEFT);
+    app.doAction ("case", "sys2");
+    
+    doc.artLayers["eye_right"].move ( doc.artLayers["main1"],ElementPlacement.PLACEBEFORE);
+    doc.artLayers["eye_left"].move ( doc.artLayers["main1"],ElementPlacement.PLACEBEFORE);
+    doc.artLayers["adjust"].move ( doc.artLayers["main1"],ElementPlacement.PLACEBEFORE);
+    CloseDoc (picDoc1);
+    return doc;
+}
+
 //MH_OpenTemplate  ("C:/i4s.tif");
 function work()
 {
     var d;
     d = MH_GetCaseInfo ();
-    for (var k in d){
+    var k;
+    for ( k = 0; k < d.length; k ++){
         var m = d[k];
-        doc = MH_OpenTemplate (m["srcPath"]);
-        
-        var targetFile = new File (m["targetPath"]);
-        doc.saveAs(targetFile, GetSaveParam ("jpg"), true);
-        CloseDoc(doc);
+        try{
+            doc = MH_OpenTemplate (m["srcPath"]);
+            
+            var targetFile = new File (m["targetPath"]);
+            doc.saveAs(targetFile, GetSaveParam ("jpg"), true);
+            CloseDoc(doc);
+            }
+       catch(err){
+            continue;}
     }
 }
 
-work();
+function work2()
+{
+    var d;
+    d = MH_GetCaseInfo ();
+    var k;
+    for ( k = 0; k < d.length; k ++){
+        var m = d[k];
+        if (k + 1 >= d.length)return ;
+        var n = d[k + 1]
+        doc = MH_OpenTemplate2 (m["srcPath"], n["srcPath"]);
+
+        var dir = Utils_GetDirectoryPathFromPath (m["targetPath"]);
+        var nameLeft = Utils_GetFileNameNoExtFromPath (m["targetPath"]);
+        var nameRight = Utils_GetFileNameNoExtFromPath (n["targetPath"]);
+        
+        var targetFile = new File (dir +"/" + nameLeft + " " + nameRight + ".jpg");
+        doc.saveAs(targetFile, GetSaveParam ("jpg"), true);
+        CloseDoc(doc);
+        k = k + 1;
+    }
+}
+
+work2();
